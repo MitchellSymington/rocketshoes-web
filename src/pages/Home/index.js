309bf7df66acbf48a51f8c7c05ae48da5640c572
mitch.palha@gmail.com <admin@MacBook-Pro.local>
2020-10-23 14:37:24 -0300

    subir projeto web bootcamp

---
+++ src/pages/Home/index.js
@@ -0,0 +1,80 @@
+import React, {useState, useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+import { ProductList, LoadingIcon, Container } from './styles';
+import * as CartActions from '../../store/modules/cart/actions';
+
+export default function Home () {
+  const [products, setProducts] = useState([]);
+
+  const amount = useSelector( state =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+
+    return sumAmount;
+  }, {})
+  );
+
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const updating = useSelector(state => state.updating);
+
+  useEffect (() => {
+    async function loadProducts(){
+      setLoading(true);
+      const response = await api.get('/products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+      setProducts(data);
+      setLoading(false);
+    }
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id) {
+    dispatch(CartActions.addToCartRequest(id));
+  };
+
+    if (loading) {
+      return (
+        <Container>
+          <strong>Carregando</strong>
+          <LoadingIcon size={22} color="#FFF" />
+        </Container>
+      );
+    }
+    return (
+      <ProductList length={products.length}>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button
+              type="button"
+              onClick={() => handleAddProduct(product.id)}
+            >
+              <div>
+                {updating.id === product.id && updating.status ? (
+                  <LoadingIcon size={16} color="#FFF" />
+                ) : (
+                  <MdAddShoppingCart size={16} color="#FFF" />
+                )}
+
+                {amount[product.id] || 0}
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
